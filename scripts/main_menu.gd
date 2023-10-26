d55a18a84fe714a65d95e9b0ac9341bee5a5b9ee
Szymon Nidecki <szynid19@gmail.com>
2023-10-26 20:41:35 +0200

    scrolling main menu fix

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,14 @@
-extends Control
+extends ParallaxBackground
+
+@export var scrolling_speed := 160
 
 
 func _ready() -> void:
 	AudioPlayer.play_music("main_menu")
+
+
+func _process(delta: float) -> void:
+	scroll_offset.x -= scrolling_speed * delta
 
 
 func _input(event: InputEvent) -> void:
